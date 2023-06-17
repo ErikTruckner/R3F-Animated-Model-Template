648e7c32126b436e7b42458897d6d4212ddbd870
Erik Truckner <eriktruckner@comcast.net>
2023-06-17 13:53:42 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import FantasyBook from './modelComponents/FantasyBook'
+import CameraPositionLogger from './helpers/CameraPositionLogger'
 
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas camera={{ fov: 75, near: 1, far: 1000, position: [7, 15, 60] }}>
         <color attach='background' args={['black']} />
         <OrbitControls />
+        <CameraPositionLogger event='mousedown' />
         <ambientLight />
         <FantasyBook />
       </Canvas>
